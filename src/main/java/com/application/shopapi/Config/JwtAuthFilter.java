ded0398e78c3afa32fc7a05037eefffce013dd50
Ashutosh <ashutoshbhattarai90@gmail.com>
2023-03-19 14:51:07 +0545

    JWT config finished(add extra according to needs)

--- src/main/java/com/application/shopapi/Config/JwtAuthFilter.java
+++ src/main/java/com/application/shopapi/Config/JwtAuthFilter.java
@@ -6,28 +6,46 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+
 @Component
 @RequiredArgsConstructor
 public class JwtAuthFilter extends OncePerRequestFilter {
     @Autowired
     private JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
     @Override
-    protected void doFilterInternal(HttpServletRequest request
-            , HttpServletResponse response
-            , FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal
+            (HttpServletRequest request,HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
         final String username;
-        if (authHeader == null || !authHeader.startsWith("Bearer "))
-        {
-            filterChain.doFilter(request,response);
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
             return;
         }
         jwt = authHeader.substring(7);
         username = jwtService.getUsername(jwt);
+        if (username != null || SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails details = this.userDetailsService.loadUserByUsername(username);
+            if (jwtService.isTokenValid(jwt, details)) {
+                UsernamePasswordAuthenticationToken authenticationToken =
+                        new UsernamePasswordAuthenticationToken
+                                (details, null, details.getAuthorities());
+                authenticationToken.setDetails(new WebAuthenticationDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+        }
+        filterChain.doFilter(request,response);
     }
 }
--- src/main/java/com/application/shopapi/Config/JwtService.java
+++ src/main/java/com/application/shopapi/Config/JwtService.java
@@ -1,10 +1,67 @@
 package com.application.shopapi.Config;
 
+import com.application.shopapi.User.UserModel;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import java.security.Key;
+import java.util.*;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
+
     public String getUsername(String jwt) {
-        return null;
+        return extractClaims(jwt, Claims::getSubject);
     }
+
+    public String generateToken(UserDetails user) {
+        return generateToken(new HashMap<>(), user);
+    }
+
+    public String generateToken(Map<String, Object> extraClaims, UserDetails user) {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(user.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(SignatureAlgorithm.HS256, getSignInKey())
+                .compact();
+    }
+
+    public boolean isTokenValid(String token,UserDetails user)
+    {
+        String username =getUsername(token);
+        return (username.equals(user.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        Date expirationDate = extractClaims(token,Claims::getExpiration);
+        return expirationDate.before(new Date());
+    }
+
+    public <T> T extractClaims(String token, Function<Claims, T> claimResolver) {
+        Claims claims = getTokenDetails(token);
+        return claimResolver.apply(claims);
+    }
+
+    private Claims getTokenDetails(String token) {
+        return Jwts
+                .parser()
+                .setSigningKey(getSignInKey())
+                .parseClaimsJwt(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        Base64.Decoder decoder = Base64.getUrlDecoder();
+        String SECRET_KEY = "655368566D597133743677397A244226452948404D635166546A576E5A723475";
+        byte[] key = decoder.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(key);
+    }
+
 }
